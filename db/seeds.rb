a0f508058db9b18e02ad0781f8089e696654200e
Bozena11 <bozena.vuckovic@gmail.com>
2023-04-17 11:27:50 +0200

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# create one student
+
+student = Student.create(
+	first_name: 'Franky',
+	last_name: 'Doyle',
+	email: 'frenky@test.au'
+)
+
+# create more fake seed data at once
+
+20.times do |i|
+	puts "Creating student #{i+1}"  #like console.log
+	Student.create(
+		first_name: "Student #{i+1}",
+		last_name: "Lname #{i+1}",
+		email: "student#{i+1}@test.au"
+	)
+end
